4ff9f16fe8d281102e48f3bbb60d2a19c8f4d831
Mertcan Yigin <mertcan.yigin@tum.de>
2015-09-21 16:19:00 +0200

    first commit

---
+++ textTranslator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  textTranslator
+//
+//  Created by Mertcan Yigin on 9/21/15.
+//  Copyright © 2015 Mertcan Yigin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
